2c8ce3aad6ef439727e706b599968e51953650f8
ThaliaCS <thalia.schone@gmail.com>
2020-04-19 19:50:31 -0300

    Front-end

--- main.js
+++ main.js
@@ -5,11 +5,7 @@ var buttonElement = document.querySelector('#app button')
 var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 function renderTodos(){
 
-    
-
     listElement.innerHTML = '';
-
-
 
     for(todo of todos ){
         var todoElement = document.createElement('li');
@@ -43,8 +39,6 @@ function addTodo(){
     renderTodos();
 
     saveToStorage();
-
-
 }
 
 buttonElement.onclick = addTodo;
